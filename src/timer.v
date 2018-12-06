8b317b86038487718491bd1c927699ff8da2771d
- Devanshi Pratap <dpratap2@siebl-0222-14.ews.illinois.edu>
2018-12-06 16:53:58 -0600

    fixed timer.v

--- src/timer.v
+++ src/timer.v
@@ -2,24 +2,30 @@ module timer (
     input clk,
     input reset,
     input enable,
-    input [3:0] currTimeLimit,
     output [3:0] newTime
     );
 
-    wire [3:0] clockCounter;
-    wire [3:0] currTime;
+    reg [3:0] clockCounter;
+    // wire [3:0] currTime;
+    reg [3:0] currTime;
+
+    always @ (posedge enable)
+    begin
+        currTime <= 0;
+        clockCounter <= 0;
+    end
 
     always @ (posedge clk)
     begin
         if (reset == 1'b1)
             begin
-                currTime = 0;
-                clockCounter = 0;
+                currTime <= 0;
+                clockCounter <= 0;
             end
-        else if (clockCounter == 50000000)
+        else if (clockCounter == 1 && enable == 1'b1)
             begin
                 currTime += 1;
-                clockCounter = 1;
+                clockCounter <= 1;
             end
         else
             begin
@@ -27,6 +33,6 @@ module timer (
             end
     end
 
-    assign newTime = currTimeLimit - currTime;
+    assign newTime = currTime;
 
 endmodule // timer
